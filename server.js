687cdfb599efde953da7c1c4797fd09db8c9dd7d
alejandro agustin collado <alejandro.agustincollado@gmail.com>
2022-11-16 20:58:44 -0300

    version cinco

--- server.js
+++ server.js
@@ -11,16 +11,36 @@ app.use(express.static('FormularioHtml'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-router.post('/send', (req, res) => {
-    console.log
+
+router.post('/', (req, res) => {
+   res.json({nombre: 'juan'})
     const transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
         port: 587,
+        secure: false,
         auth: {
             user:'jimmy.hodkiewicz@ethereal.email',
             pass:'JXJZE4yVCJ9eW1Rgqe'
         }
     })
+
+    const mailOptions = {
+        from: 'Nodemailer test>',
+        to: 'agus_coll@hotmail.com',
+        subject: 'Super sorteo mundialista en Ort',
+        text: 'Saludos afortunado, has ganado un maravillo premio, promocionar la materia!',
+        //html: '<p><b>Hello</b> to myself!</p>'
+    
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) { 
+            console.log('Error occurred. ' + err.message); 
+            return process.exit(1); 
+        } 
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
 })
 
 /*Name	Jimmy Hodkiewicz
@@ -36,28 +56,10 @@ transporter = nodemailer.createTransport({
 });
 */
 
-const mailOptions = {
-    from: 'Nodemailer test>',
-    to: 'han.adolfo@gmail.com',
-    subject: 'Super sorteo mundialista en Ort',
-    text: 'Saludos afortunado, has ganado un maravillo premio, promocionar la materia!',
-    //html: '<p><b>Hello</b> to myself!</p>'
-
-}
-
-
-transporter.sendMail(message, (err, info) => {
-        if (err) { 
-            console.log('Error occurred. ' + err.message); 
-            return process.exit(1); 
-        } 
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
 
 
 app.use('/api/participantes.js', new RouterParticipantes().start())
-
+app.use('/send', router)
 const PORT = config.PORT
 const server = app.listen(PORT, () => console.log(`Servidor escuchando puerto: ${PORT}`))
 server.on('error', error => console.log(`Aca esta el error: ${error.message}`))
